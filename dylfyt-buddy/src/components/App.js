e4fa5bf16b11950a7aa83ca7f068c6bf9206ec89
Dylan <dylanierugan@gmail.com>
2022-11-15 14:18:47 -0600

    Dashboard Setup

--- dylfyt-buddy/src/components/App.js
+++ dylfyt-buddy/src/components/App.js
@@ -1,19 +1,27 @@
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <AuthProvider>
+    
       <Container 
       className="d-flex align-items-center justify-content-center" 
       style = {{ minHeight: "100vh"}}
       >
         <div className="w-100" style={{ maxWidth: '400px'}}>
-        <Signup />
+          <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path='/' element={<Dashboard/>} />
+              <Route path='/signup' element={<Signup/>} />
+            </Routes>
+          </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   )
 }
 
