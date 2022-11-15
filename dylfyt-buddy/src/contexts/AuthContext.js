95d6c8d3bdaed47c3af2c79e963d47f8351693a2
Dylan <dylanierugan@gmail.com>
2022-11-15 16:04:02 -0600

    Dashboard Component

--- dylfyt-buddy/src/contexts/AuthContext.js
+++ dylfyt-buddy/src/contexts/AuthContext.js
@@ -1,45 +1,62 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { auth } from '../firebase'
+import React, { useContext, useState, useEffect } from "react"
+import { auth } from "../firebase"
 
 const AuthContext = React.createContext()
 
 export function useAuth() {
-    return useContext(AuthContext)
+  return useContext(AuthContext)
 }
 
-
 export function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState()
-    const [loading, setLoading] = useState(true)
+  const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
 
-    function signup(email, password) {
-        // creates usser in firebase
-        auth.createUserWithEmailAndPassword(email, password)
-    }
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
 
-    function login(email, password) {
-        return auth.signInWithEmailAndPassword(email, password)
-      }
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
+  }
 
-    useEffect(() => {
-        // listen for change in user
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user)
-            setLoading(false)
-        })
-        // stops listening to listener
-        return unsubscribe
-    }, [])
-    
-    const value = {
-        currentUser,
-        signup,
-        login
-    }
-    
-    return (
+  function logout() {
+    return auth.signOut()
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email)
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email)
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword
+  }
+
+  return (
     <AuthContext.Provider value={value}>
-        {!loading && children}
+      {!loading && children}
     </AuthContext.Provider>
   )
 }
