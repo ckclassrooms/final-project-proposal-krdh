48f5e74280c26c69ca72e2dda8de3cd2ef563c7d
rkap5423 <rkap5423@gmail.com>
2022-11-16 16:07:50 -0600

    consolelog

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+console.log("test here")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
