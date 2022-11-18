3115906fde529ee63064e5e73925c1193682c72c
kashah331 <83794145+kashah331@users.noreply.github.com>
2022-11-17 21:45:18 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // testing
 
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './components/App';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
